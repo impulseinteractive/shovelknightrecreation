44194ceda3713eeb2e3c20322cf76905953f12ac
hd-tran <36580646+hd-tran@users.noreply.github.com>
2026-06-27 12:15:13 -0700

    global/autoload level state manager script with signals to connect and emit for handling state transitions
    - Current LevelStates: INITIAL, PLAY, CUTSCENE, LEVEL_SUCCESS, LEVEL_FAILURE
    - Connect to state_entered and state_exit signals with local scenes and objects to handle their own state cleanup / setup
    - Emit state_changed signal when a state transition needs to occur to trigger a state transition, will then emit a state_exit and state_enter signal for anything that needs to cleanup / setup for a new state

---
+++ shovelknightrecreation/Scripts/Global/level_state_manager.gd
@@ -0,0 +1,30 @@
+extends Node
+
+
+enum LevelState{INITIAL, PLAY, CUTSCENE, LEVEL_SUCCESS, LEVEL_FAILURE}
+
+## Emit this signal with new LevelState enum value to trigger transition to a new state
+signal state_changed(new_state: LevelState)
+
+## Connect to this signal to handle any initial state setup after transitioning into a new state
+signal state_entered(new_state: LevelState)
+
+## Connect to this signal to handle any state cleanup before transitioning into a new state completes
+signal state_exited(old_state: LevelState)
+
+## Current level state value
+var LEVEL_STATE: LevelState
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	LEVEL_STATE = LevelState.INITIAL
+	state_changed.connect(on_state_changed)
+	return
+
+
+func on_state_changed(new_state: LevelState):
+	state_exited.emit(LEVEL_STATE)
+	LEVEL_STATE = new_state
+	state_entered.emit(LEVEL_STATE)
+	return
