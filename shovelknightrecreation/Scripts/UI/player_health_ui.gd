411bc9a33c28d5b6e87d56031cb8927c3acb8d23
hd-tran <36580646+hd-tran@users.noreply.github.com>
2026-06-28 15:04:50 -0700

    bug fix: fixed player and knlack knight health bubbles not emptying after losing health, now properly shows empty bubbles

--- shovelknightrecreation/Scripts/UI/player_health_ui.gd
+++ shovelknightrecreation/Scripts/UI/player_health_ui.gd
@@ -14,12 +14,13 @@ var ui_current_health: int = ui_max_health
 
 @export var debug_input: bool = false
 
-signal on_player_health_changed(new_health: int)
+#signal on_player_health_changed(new_health: int)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	on_player_health_changed.connect(update_player_health)
+	#on_player_health_changed.connect(update_player_health)
+	player_ref.on_health_changed.connect(update_player_health)
 	
 	if player_ref:
 		ui_max_health = player_ref.max_health
@@ -36,12 +37,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if debug_input and Input.is_action_just_pressed("ui_page_down"):
 		ui_current_health -= 1
-		on_player_health_changed.emit(ui_current_health)
+		player_ref.on_health_changed.emit(ui_current_health)
 		pass
 	pass
 
 
 func update_player_health(new_health: int):
+	ui_current_health = new_health
 	var life_bubbles = life_bar_container.get_children()
 	for idx in ceil(ui_max_health / 2):
 		if idx < ceil(ui_current_health / 2):
@@ -51,8 +53,8 @@ func update_player_health(new_health: int):
 			var texture_bubble: TextureRect = life_bubbles.get(idx)
 			texture_bubble.texture = life_bubble_empty
 	
-	if new_health % 2 != 0:
+	if ui_current_health % 2 != 0:
 		print("Odd")
-		var last_bubble: TextureRect = life_bubbles.get(ceil(new_health / 2))
+		var last_bubble: TextureRect = life_bubbles.get(ceil(ui_current_health / 2))
 		last_bubble.texture = life_bubble_half
 	return
