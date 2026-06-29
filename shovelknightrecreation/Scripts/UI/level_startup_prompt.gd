fe027d4d0f94d2c5575edc8038db7d1879ea94f6
Popt0 <109549588+Popt0@users.noreply.github.com>
2026-06-28 19:39:37 -0700

    Black Knight faces correct direction

--- shovelknightrecreation/Scripts/UI/level_startup_prompt.gd
+++ shovelknightrecreation/Scripts/UI/level_startup_prompt.gd
@@ -4,31 +4,37 @@ extends Control
 @onready var level_manager = LevelStateManager
 @onready var press_enter_to_start: RichTextLabel = %PressEnterToStart
 
+var started: bool = false
+var ran: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	level_manager.state_exited.connect(_on_exit_initial)
 	level_manager.state_entered.connect(_on_enter_initial)
-	
-	get_tree().paused = true
-	grab_focus()
-	press_enter_to_start.show()
 
 	return
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if started and not ran:
+		get_tree().paused = true
+		grab_focus()
+		press_enter_to_start.show()
+		ran = true	
+	else:
+		started = true
+	
 	if Input.is_action_just_pressed("enter"):
+		print_debug("ENTER")
 		level_manager.state_changed.emit(LevelStateManager.LevelState.PLAY)
 		return
 	return
 
 func _on_enter_initial(new_state: LevelStateManager.LevelState):
 	if new_state == LevelStateManager.LevelState.INITIAL:
-		get_tree().paused = true
-		grab_focus()
-		press_enter_to_start.show()
+		ran = false
+		started = false
 	return
 
 func _on_exit_initial(old_state: LevelStateManager.LevelState):
